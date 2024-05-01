77a33025e7dce028f36278fdfd6c629e25b4f8f1
Henry Le <lehieu2077@gmail.com>
2024-05-01 15:56:58 +0700

    chore: split files

---
+++ controller/handlers.go
@@ -0,0 +1,44 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// ---------------------- RouteHandler ----------------------
+type RouteHandler struct {
+	method string
+	pattern string
+	handler func(w http.ResponseWriter, r *http.Request)
+}
+
+type RouteHandlerError struct {
+	message string
+}
+
+func (e *RouteHandlerError) Error() string {
+	return e.message
+}
+
+func DeclareRouteHandler(method string, pattern string, handler func(w http.ResponseWriter, r *http.Request)) *RouteHandler {
+	if method == "" {
+		method = "GET"
+	}
+
+	if pattern == "" {
+		pattern = "/"
+	}
+
+	return &RouteHandler{method: method, pattern: pattern, handler: handler}
+}
+
+func (rh * RouteHandler) Resolve(router * mux.Router) error {
+	r := router.HandleFunc(rh.pattern, rh.handler).Methods(rh.method)
+
+	if r == nil {
+		return &RouteHandlerError{message: "Failed to resolve route handler"}
+	}
+
+	return nil
+}
