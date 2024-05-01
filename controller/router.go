ff17cbb21b2d71d8adc2186acd40b7ebabd70161
Henry Le <lehieu2077@gmail.com>
2024-05-01 15:46:32 +0700

    feat: update name

--- controller/router.go
+++ controller/router.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// ---------------------- RouteHandler ----------------------
 type RouteHandler struct {
 	method string
 	pattern string
@@ -21,6 +22,14 @@ func (e *RouteHandlerError) Error() string {
 }
 
 func DeclareRouteHandler(method string, pattern string, handler func(w http.ResponseWriter, r *http.Request)) *RouteHandler {
+	if method == "" {
+		method = "GET"
+	}
+
+	if pattern == "" {
+		pattern = "/"
+	}
+
 	return &RouteHandler{method: method, pattern: pattern, handler: handler}
 }
 
@@ -29,6 +38,34 @@ func (rh * RouteHandler) Resolve(router * mux.Router) error {
 
 	if r == nil {
 		return &RouteHandlerError{message: "Failed to resolve route handler"}
+	}
+
+	return nil
+}
+
+// ---------------------- Routers ----------------------
+/*
+	Declare a new router, in which there are many route handlers
+*/
+
+func DeclareRouter() IRouter {
+	return &Router{}
+}
+
+type Router struct {
+	Handlers []*RouteHandler
+}
+
+func (r * Router) Add(handler *RouteHandler) {
+	// Add handler to handlers
+	r.Handlers = append(r.Handlers, handler)
+}
+
+func (r * Router) Resolve(router * mux.Router) error {
+	for _, h := range r.Handlers {
+		if err := h.Resolve(router); err != nil {
+			return err
+		}
 	}
 
 	return nil
--- controller/struct.go
+++
@@ -1,9 +0,0 @@
-package controller
-
-import "net/http"
-
-type ControllerType struct {
-	method string
-	route string
-	handler http.Handler
-}
