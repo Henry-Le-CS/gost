bcef1f6eff579bc9bea50c2d55f7d2bd08ad9a8a
Henry Le <lehieu2077@gmail.com>
2024-05-01 16:26:52 +0700

    chore: update message for handler

--- controller/handlers.go
+++ controller/handlers.go
@@ -18,7 +18,7 @@ type RouteHandlerError struct {
 }
 
 func (e *RouteHandlerError) Error() string {
-	return e.message
+	return "[Route handler]: " + e.message
 }
 
 // Declare RouteHandler declare a new route handler
@@ -38,8 +38,9 @@ func DeclareRouteHandler(method string, pattern string, handler func(w http.Resp
 func (rh * RouteHandler) Resolve(router * mux.Router) error {
 	r := router.HandleFunc(rh.pattern, rh.handler).Methods(rh.method)
 
-	if r == nil {
-		return &RouteHandlerError{message: "Failed to resolve route handler"}
+	if r != nil {
+		msg := "Failed to resolve route handler for pattern: " + rh.pattern + " and method: " + rh.method
+		return &RouteHandlerError{message: msg}
 	}
 
 	return nil
